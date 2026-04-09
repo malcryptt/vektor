e256c3522880185907df4fb30dcf4822a8df40f8
malcryptt <tsmayor74@gmail.com>
2026-04-09 14:55:18 +0100

    Update: Increase HeroTerminal animation speed to 15ms

--- frontend/src/components/HeroTerminal.tsx
+++ frontend/src/components/HeroTerminal.tsx
@@ -21,7 +21,7 @@ export default function HeroTerminal() {
     useEffect(() => {
         const timer = setInterval(() => {
             setVisibleLines(prev => (prev < AUDIT_LINES.length - 1 ? prev + 1 : 0));
-        }, 800);
+        }, 15);
         return () => clearInterval(timer);
     }, []);
 
